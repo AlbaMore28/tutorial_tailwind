6c44dca58105245494010a805c5a671aa17c798d
Alba Moreno <moreno.alba.97@gmail.com>
2022-04-24 14:43:35 +0200

    Resizing a image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,16 +8,8 @@
     <title>Tutorial Tailwind</title>
 </head>
 <body>
-    <div class="contenedor">
-        <img class="float-right" src="https://images.pexels.com/photos/4217987/pexels-photo-4217987.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
-        <img class="float-left" src="https://images.pexels.com/photos/11672681/pexels-photo-11672681.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
-        <p class="mb-2 clear-both">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates omnis blanditiis reprehenderit labore animi quo dolores rerum at asperiores ipsam, ipsa quod iure obcaecati earum. Possimus ad repellendus accusamus in?</p>
-        <p class="mb-2">Autem ut et dolorum temporibus necessitatibus enim fuga beatae doloremque maxime ipsum, quas assumenda excepturi molestias quibusdam, totam consequuntur fugiat molestiae asperiores corporis velit! Impedit, sunt illo. Maiores, perferendis at?</p>
-        <p class="mb-2">Commodi quidem deserunt obcaecati ipsa sed, ea aperiam autem libero itaque laborum quod eligendi ab error voluptatum. Tenetur hic doloribus a. Rem id ut assumenda repudiandae vitae eaque quaerat sapiente?</p>
-        <p class="mb-2">Dolore vel officiis porro illum, expedita quod quae fugiat architecto aspernatur nulla qui voluptas quasi laudantium voluptatum accusantium rerum voluptate illo nisi quaerat, impedit enim ad? Rerum cupiditate laborum veniam.</p>
-        <p class="mb-2">Quam culpa esse eligendi, accusantium fugit a hic. Deserunt illum dolorem, vero esse natus odit nostrum facere necessitatibus laborum voluptas fuga quisquam officia quo pariatur omnis facilis, cumque id architecto.</p>
-        <p class="mb-2">Quam culpa esse eligendi, accusantium fugit a hic. Deserunt illum dolorem, vero esse natus odit nostrum facere necessitatibus laborum voluptas fuga quisquam officia quo pariatur omnis facilis, cumque id architecto.</p>
-        <p class="mb-2">Quam culpa esse eligendi, accusantium fugit a hic. Deserunt illum dolorem, vero esse natus odit nostrum facere necessitatibus laborum voluptas fuga quisquam officia quo pariatur omnis facilis, cumque id architecto.</p>
+    <div class="contenedor bg-gray-300">
+        <img class="w-full h-64 object-cover" src="https://cdn.pixabay.com/photo/2022/03/09/16/30/dog-7058195_960_720.jpg" alt="">
     </div>
 </body>
 </html>
