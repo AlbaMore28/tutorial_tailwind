4e28000ae828225598161980a1901b605cee2c55
Alba Moreno <moreno.alba.97@gmail.com>
2022-04-24 13:02:28 +0200

    Adding full round border Adding paragraphs divided by horizontal lines Adding menu divided by vertical lines

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,23 @@
 </head>
 <body>
     <div class="contenedor pt-5">
-        <div class="w-64 h-64 bg-gray-500 border-b-8 border-blue-800 hover:border-opacity-75"></div>
+        <div class="w-64 h-32 bg-gray-500 border-8 border-blue-800 rounded-full"></div>
+
+        <div class="divide-y divide-gray-600 divide-dashed"> 
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas commodi esse modi natus perspiciatis animi quis molestiae pariatur ab autem illum distinctio quisquam exercitationem quasi cupiditate, eligendi et temporibus ullam.</p>
+            <p>Perspiciatis corrupti illum minima, accusantium enim eaque dolores tenetur temporibus quaerat nobis nihil doloribus consequuntur vel provident doloremque! Veritatis nemo architecto tempora error, voluptatibus deserunt. Vero nobis sit asperiores atque!</p>
+            <p>Ipsam impedit perferendis, dolorem recusandae soluta earum eligendi excepturi placeat at hic vitae commodi labore odio reprehenderit non sequi ipsum atque, odit libero accusamus alias. Quod, sed voluptates? Voluptatem, eveniet.</p>
+            <p>Delectus iusto eaque ad itaque, libero ullam repudiandae dicta reprehenderit possimus laborum quaerat dolor exercitationem quidem blanditiis eligendi ipsa deleniti commodi quasi? Incidunt, nam nesciunt quia commodi vero sapiente minima!</p>
+            <p>Maxime unde id quibusdam, quisquam error vero inventore autem modi iste consequatur eos in voluptatibus voluptate ad repellendus vitae odit officiis illo nesciunt cupiditate? Totam maiores nam autem quam perspiciatis?</p>
+        </div>
+
+    <nav class="divide-x-2 divide-blue-600">
+        <a href="">Link 1</a>
+        <a href="">Link 2</a>
+        <a href="">Link 3</a>
+        <a href="">Link 4</a>
+        <a href="">Link 5</a>
+    </nav>
     </div>
 </body>
 </html>
