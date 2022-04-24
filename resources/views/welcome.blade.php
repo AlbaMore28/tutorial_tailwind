a16a710dced6b829d5ed68dede096adaaf6ef23f
Alba Moreno <moreno.alba.97@gmail.com>
2022-04-24 14:16:55 +0200

    Adding styles to div

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,33 +9,7 @@
 </head>
 <body>
     <div class="contenedor">
-        
-        <table class="table w-full border-separate lg:border-collapse table-fixed">
-            <thead>
-                <tr>
-                    <th class="w-1/4">Pais</th>
-                    <th class="w-1/4">Ciudad</th>
-                    <th class="w-2">Descripción</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Perú</td>
-                    <td>Lima</td>
-                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda asperiores incidunt dolorum hic earum ab laboriosam amet dolore voluptatibus quibusdam sequi blanditiis, veritatis facere autem harum fugit exercitationem deleniti nam?</td>
-                </tr>
-                <tr>
-                    <td>Colombia</td>
-                    <td>Bogota</td>
-                    <td>Natus maxime deleniti nemo porro perspiciatis dicta illo ullam quasi corrupti expedita. Harum nulla animi corrupti sit totam. Aperiam unde nihil provident ratione rem reiciendis debitis. Odit in illum harum?</td>
-                </tr>
-                <tr>
-                    <td>España</td>
-                    <td>Madrid</td>
-                    <td>Voluptatum in, suscipit necessitatibus ullam temporibus distinctio inventore. Quod repellendus laudantium assumenda vero maxime laborum a eveniet aliquid similique dolorum modi sapiente incidunt iure asperiores, tempore cupiditate accusantium. Nobis, porro!</td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="bg-green-600 h-32 w-screen"></div>
     </div>
 </body>
 </html>
