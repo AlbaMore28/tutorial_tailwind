9e5fa7c2c47657316eb355420234e0ef5d3586b8
Alba Moreno <moreno.alba.97@gmail.com>
2022-04-24 12:30:59 +0200

    Adding image in a div

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,19 +7,21 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/contenedor.css') }}" rel="stylesheet">
     <title>Document</title>
+    <style>
+        .imagen{
+            background-image: url("{{asset('img/perro1.jpg')}}");
+            height: 400px;
+            width: 400px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
 </head>
 <body>
     <div class="contenedor">
-        <h1 class="text-xl font-bold text-left md:text-center lg:text-right underline uppercase">Este es el título</h1>
-        <p class="text-red-700 hover:text-opacity-75">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, neque eos aspernatur nulla quaerat recusandae provident laborum quod veniam tempore eaque velit sed impedit. Delectus rem dolorem quidem quas omnis.</p>
-        <ul class="list-decimal md:list-disc list-inside">
-            <li class="line-through">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat vitae nesciunt error delectus quod possimus veritatis sed, temporibus molestias, natus blanditiis tempore doloribus adipisci deserunt, et aliquam quas quisquam ab.</li>
-            <li>Dolore architecto ab doloremque consectetur itaque accusamus consequuntur vel vitae magnam incidunt iste, ipsa distinctio, amet, at ut dolor! Esse incidunt optio, minus fuga expedita error iusto. Aliquid, itaque optio.</li>
-            <li>Enim sed asperiores a obcaecati placeat quia recusandae eaque est natus expedita rem, reiciendis repellat impedit minima cumque. Doloremque totam obcaecati libero quo maxime recusandae voluptate officia, porro non quibusdam.</li>
-            <li>Quod repellat ad dolor sit ab quas veritatis expedita corrupti dolore iure vero sunt voluptate numquam inventore impedit esse beatae porro, officiis accusamus totam culpa adipisci. Quidem mollitia magnam corrupti!</li>
-            <li class="lowercase">Possimus dolorum neque rem suscipit dolores ratione enim blanditiis vel temporibus, a quos? Optio atque dolore molestiae aut possimus architecto recusandae illum? Mollitia vel explicabo, aut soluta sequi eos itaque.</li>
-        </ul>
-        <p class="capitalize">alba moreno peinado</p>
+        <h1 class="text-center text-3xl font-bold mb-3">Background</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, obcaecati asperiores eligendi ex aliquid omnis nobis debitis velit! Minus totam explicabo temporibus eligendi alias dolore ab minima amet reprehenderit inventore.</p>
+        <div class="imagen bg-contain"></div>
     </div>
 </body>
 </html>
