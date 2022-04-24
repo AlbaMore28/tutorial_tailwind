2250d6233c8494a1b290e1cb8e6cf59b22781996
Alba Moreno <moreno.alba.97@gmail.com>
2022-04-24 15:36:41 +0200

    Adding fixed positioning to nav and div

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,19 +8,33 @@
     <title>Tutorial Tailwind</title>
 </head>
 <body>
-    <div class="contenedor mt-4">
-        <div class="bg-gray-200 p-4 relative">
-            <div class="bg-gray-400 p-4">
-                <div class="bg-blue-500 p-4">
+    <nav class="bg-blue-300 h-16 fixed inset-x-0 z-50">
 
-                </div>  
-                <div class="bg-blue-400 p-4 absolute inset-x-0 bottom-0">
+    </nav>
+    <aside class="w-64 bg-gray-800 fixed inset-y-0 z-40">
 
-                </div>
-    
-            </div>
-
-        </div>
+    </aside>
+    <div class="contenedor pt-16">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, minima ducimus porro culpa maxime repellendus rerum dicta delectus dolorum, officia tenetur quae explicabo? Iusto temporibus sit similique doloribus excepturi laudantium!</p>
+        <p>Quia, porro hic, soluta rem odit, error totam sunt facere aut ad atque. Vitae quam, odit, laboriosam dolore velit fugit impedit veritatis libero similique beatae adipisci exercitationem. Reiciendis, recusandae fugit?</p>
+        <p>Deleniti, voluptate ratione asperiores expedita molestias iure quam mollitia aspernatur sit repudiandae quo repellendus laudantium modi corporis quod, illo distinctio voluptatem? Sed eaque temporibus sunt aut magni delectus quam minus!</p>
+        <p>Provident voluptatum earum tenetur aperiam quam itaque? Esse incidunt nisi voluptates natus excepturi magni inventore, laborum recusandae nihil iste minus tenetur magnam enim, velit id impedit eligendi neque, tempore aut.</p>
+        <p>Illo ut necessitatibus nemo exercitationem, laborum error repudiandae sit inventore cum molestias. Qui quidem perspiciatis, quis rerum fugit facilis incidunt minus tempore nisi at ea iusto dolorum odio accusantium repellat.</p>
+        <p>Sint, dicta. Ut harum obcaecati eaque architecto reprehenderit nesciunt ad consequuntur cumque illum suscipit numquam nulla, deserunt odio modi consequatur a praesentium veritatis esse molestiae aspernatur, minus minima, ipsa hic.</p>
+        <p>Veritatis consequuntur, tempore impedit eveniet rerum officiis. Sed doloribus eum neque non. Consectetur dolorum repellendus deserunt eligendi eveniet ea neque! Accusantium vel amet reiciendis doloribus earum culpa quo nemo autem?</p>
+        <p>Commodi aliquam rem hic amet doloremque, recusandae eligendi alias repellat! Incidunt consequuntur quis ab temporibus blanditiis esse saepe, molestiae quos, reprehenderit possimus ut velit laudantium adipisci consequatur, rerum fuga inventore?</p>
+        <p>Magnam ducimus aperiam illum, expedita consequuntur veritatis. Dolorem, eum iusto veritatis, fugit quam in impedit hic nobis voluptatibus odit harum voluptate laborum minus nulla ab, corporis est! Cumque, quo eum.</p>
+        <p>Omnis, asperiores beatae ut a quaerat nemo deserunt ipsa, qui voluptates enim perspiciatis temporibus amet quidem fugiat reprehenderit est accusantium cum, suscipit incidunt quis ipsum illum quam! Non, itaque facere.</p>
+        <p>Iste assumenda ab maxime, itaque dignissimos est ratione modi recusandae commodi molestias a dolore nisi quasi harum cumque nostrum repellendus odit quibusdam reprehenderit cupiditate? Dolorum totam at laborum explicabo dolor?</p>
+        <p>Fugiat eos aliquid odit repellendus magni, deserunt illum facere doloremque officiis ad, esse laudantium quasi voluptate quo quaerat commodi eligendi officia, laboriosam aliquam aut! Sapiente, accusantium. Vel velit eos consequatur!</p>
+        <p>Temporibus, iusto, excepturi nobis odio quas dolore pariatur eligendi architecto, eius magni voluptates est totam nam cum? Labore reprehenderit temporibus aliquid harum maiores. Ullam beatae excepturi ad! Corporis, architecto laborum?</p>
+        <p>Accusantium autem mollitia laudantium obcaecati. Asperiores, voluptas eveniet, delectus totam accusamus ex iste obcaecati, illo unde quo reprehenderit aliquam cumque. Vel error debitis vero enim provident praesentium minus nulla quisquam!</p>
+        <p>Quos autem temporibus ex nam placeat a maxime ratione praesentium fugiat quis voluptatem eligendi dignissimos nemo odit, rerum nihil aspernatur? Quidem quae consectetur deserunt est, non culpa porro exercitationem odio!</p>
+        <p>Pariatur, repellendus aliquam. Repellat repudiandae minus natus excepturi maiores nihil quod commodi asperiores perspiciatis esse obcaecati nisi, ratione, veniam ducimus, at alias! Quidem dolorum beatae similique quos fugit vero blanditiis.</p>
+        <p>Ut molestias nihil facere inventore accusamus similique expedita? Minima laborum iure cumque id nesciunt magnam fugiat voluptatum architecto quisquam. Deleniti et ut qui? Corporis, sit aspernatur ratione porro perspiciatis magnam.</p>
+        <p>Ipsum nisi quaerat enim saepe ab laborum aspernatur possimus quisquam consequatur quas. Vitae mollitia est quibusdam eveniet, molestias amet optio vel ut repudiandae nam molestiae quia aliquid iusto porro! Laboriosam!</p>
+        <p>Architecto error quis minima est aperiam reiciendis, quo, itaque totam asperiores odio non cupiditate reprehenderit nihil et iusto rem praesentium nulla ipsam. Nisi ipsa dolorem qui accusantium fugiat cupiditate obcaecati.</p>
+        <p>Dolor, reiciendis! Minus omnis ducimus obcaecati quaerat placeat fugiat blanditiis dicta quis pariatur reiciendis voluptate optio veritatis facere deserunt vitae, quo nihil porro. Reiciendis neque est numquam! Distinctio, consectetur deserunt?</p>
     </div>
 </body>
 </html>
